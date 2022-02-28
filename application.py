6c169e0b6a6c42f7474c807bcafbed53e8efc23b
Avuxon <sathwik45@gmail.com>
2022-02-27 22:32:00 -0500

    Add login authentication functionality

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, current_user, login_required, \
+    logout_user
+
 from wtform_fields import *
 from models import *
 
@@ -10,6 +13,14 @@ app.secret_key = 'replace later'
 #configure database
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://zixzrzxtdupbuj:1248d5efcacceb921a4ec3fede09c4320360597fa752c3fdc0aa87e837880df0@ec2-52-204-29-205.compute-1.amazonaws.com:5432/d72m5ca7955jkp'
 db = SQLAlchemy(app)
+
+# Configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -37,12 +48,28 @@ def index():
 @app.route("/login", methods=['GET', 'POST']) 
 def login():
     login_form = LoginForm()
+
     # Allow login if validation success
     if login_form.validate_on_submit():
-        return "Logged in!"
+        user_object = \
+            User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('chat'))
 
     return render_template("login.html", form=login_form)
 
+@app.route('/chat', methods=['GET', 'POST'])
+# @login_required
+def chat():
+    if not current_user.is_authenticated:
+        return "Please login before accessing chat"
+
+    return "Chat with me"
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    logout_user()
+    return "Logged out using Flask-login"
 
 if __name__ == "__main__":
     app.run(debug = True)
